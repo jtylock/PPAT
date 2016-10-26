c578c2c1102ed71959ba768c124ccc73810bfeb4
Jen Tylock <jtylock@24-1-196.dynamic.csail.mit.edu>
2016-10-26 14:35:13 -0400

    All the files Jen has been working on

---
+++ firstCameraApp/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  firstCameraApp
+//
+//  Created by Jen Tylock on 10/14/16.
+//  Copyright © 2016 Jen Tylock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var photoLibraryButton: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var imageViewButton: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func photoLibraryAction(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        
+        
+    
+    }
+    
+    @IBAction func cameraAction(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
+    }
+    
+
+}
+
